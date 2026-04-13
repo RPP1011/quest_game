3e9263f72b7639e7f050cfe385f63ffa04f6189c
Ricky <ricky@rpgrafx.com>
2026-04-12 22:28:21 -0700

    feat(engine): expose Pipeline + PipelineTrace

--- app/engine/__init__.py
+++ app/engine/__init__.py
@@ -1,18 +1,24 @@
 from .context_builder import AssembledContext, ContextBuilder
 from .context_spec import ContextSpec, EntityScope, NarrativeMode, PLAN_SPEC, WRITE_SPEC
 from .inference_params import InferenceParams, TokenUsage
+from .pipeline import BEAT_SHEET_SCHEMA, Pipeline, PipelineOutput
 from .prompt_renderer import PromptRenderer
 from .stages import StageConfig, StageError, StageResult
 from .token_budget import TokenBudget, estimate_tokens
+from .trace import PipelineTrace
 
 __all__ = [
     "AssembledContext",
+    "BEAT_SHEET_SCHEMA",
     "ContextBuilder",
     "ContextSpec",
     "EntityScope",
     "InferenceParams",
     "NarrativeMode",
     "PLAN_SPEC",
+    "Pipeline",
+    "PipelineOutput",
+    "PipelineTrace",
     "PromptRenderer",
     "StageConfig",
     "StageError",
