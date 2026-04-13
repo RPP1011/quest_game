89754834b84d8feda64d533b44e82772195825d8
Ricky <ricky@rpgrafx.com>
2026-04-12 22:24:00 -0700

    feat(engine): expose P2 public api

---
+++ app/engine/__init__.py
@@ -0,0 +1,24 @@
+from .context_builder import AssembledContext, ContextBuilder
+from .context_spec import ContextSpec, EntityScope, NarrativeMode, PLAN_SPEC, WRITE_SPEC
+from .inference_params import InferenceParams, TokenUsage
+from .prompt_renderer import PromptRenderer
+from .stages import StageConfig, StageError, StageResult
+from .token_budget import TokenBudget, estimate_tokens
+
+__all__ = [
+    "AssembledContext",
+    "ContextBuilder",
+    "ContextSpec",
+    "EntityScope",
+    "InferenceParams",
+    "NarrativeMode",
+    "PLAN_SPEC",
+    "PromptRenderer",
+    "StageConfig",
+    "StageError",
+    "StageResult",
+    "TokenBudget",
+    "TokenUsage",
+    "WRITE_SPEC",
+    "estimate_tokens",
+]
